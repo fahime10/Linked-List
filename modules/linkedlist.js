52e3c3abad9191a52e696623ef6fc84b8c6f96c2
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-21 22:55:30 +0100

    Implemented at, pop, contains, find, toString

--- modules/linkedlist.js
+++ modules/linkedlist.js
@@ -47,4 +47,80 @@ export class LinkedList {
 
         return current.value;
     }
+
+    at(index) {
+        if (index < 0) {
+            return null;
+        } else {
+            let current = this.head;
+            
+            while (index > 0) {
+                current = current.nextNode;
+                index--;
+            }
+
+            return current.value;
+        }
+    }
+
+    pop() {
+        let current = this.head;
+
+        while (current.nextNode.nextNode) {
+            current = current.nextNode;
+        }
+
+        current.nextNode = null;
+    }
+
+    contains(value) {
+        let current = this.head;
+        let result = false;
+
+        while (current.nextNode) {
+            if (current.value === value) {
+                result = true;
+                return result;
+            }
+
+            current = current.nextNode;
+        }
+
+        return result;
+    }
+
+    find(value) {
+        let current = this.head;
+        let index = 0;
+
+        while (current.nextNode) {
+            if (current.value === value) {
+                return index;
+            } else {
+                index++;
+                current = current.nextNode;
+            }
+        }
+
+        if (current.value !== value) {
+            return null;
+        } else {
+            return index;
+        }
+    }
+
+    toString() {
+        let current = this.head;
+        let string = "";
+
+        while (current.nextNode) {
+            string += `( ${current.value} ) -> `;
+            current = current.nextNode;
+        }
+
+        string += `( ${current.value} ) -> `;
+        string += " null ";
+
+        return string;
+    }
 }
